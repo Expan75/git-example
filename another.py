997769d1bccfa11da34bd89a4f6a2ef475940e6c
hakan <erik.hakansson96@gmail.com>
2022-06-14 20:35:12 +0200

    made comments

--- another.py
+++ another.py
@@ -1,1 +1,12 @@
 # write something
+
+# Misc commands: 
+# - git status
+# - git log
+# - timetravel with: git checkout <hash>
+
+# 1. add <filename> (only required if new files)
+
+# 2. commit <filename> -m "my message"
+
+# 3. 
