f2bef01da3912ced7a88d66b5ce443e8a1bd083e
hakan <erik.hakansson96@gmail.com>
2022-06-14 20:47:35 +0200

    added remote connenction instructions

--- another.py
+++ another.py
@@ -4,9 +4,14 @@
 # - git status
 # - git log
 # - timetravel with: git checkout <hash>
+# - undo timetravel: git switch - 
 
+# Setup remote connection
+# - git remote add origin git@github.com:Expan75/git-example.git
+# - git remote
+# - git branch -M main
+
+# GIT FLOW
 # 1. add <filename> (only required if new files)
-
 # 2. commit <filename> -m "my message"
-
 # 3. 
